0a7c48644d92066cf31a3991dc25a70bcc2358e8
Simon de Haan <simon@praekeltfoundation.org>
2011-12-20 20:06:22 +0200

    redis key helper

--- qawa/redis_utils.py
+++ qawa/redis_utils.py
@@ -1,17 +1,25 @@
 import redis
 
+
+def redis_key_maker(key):
+    def generate_key(*args):
+        return ':'.join([key] + map(str, args))
+    return generate_key
+
+user_key = redis_key_maker('user')
+
 class RedisStore(object):
-    
+
     def __init__(self, redis_class = redis.Redis):
         self.server = redis_class()
-    
-    def authenticate(self, username, password):        
-        user = self.server.hgetall('user:%s' % username)        
+
+    def authenticate(self, username, password):
+        user = self.server.hgetall(user_key(username))
         return user and user.get('password') == password
-    
+
     def register(self, username, password):
-        return self.server.hset('user:%s' % username, 'password', password)
-    
+        return self.server.hset(user_key(username), 'password', password)
+
     def user_exists(self, username):
-        return self.server.exists('user:%s' % username)
-    
+        return self.server.exists(user_key(username))
+
