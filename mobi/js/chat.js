c821e7838816dafd6c0a0dea6c186e140e8bd931
Peter Pistorius <peter.pistorius@gmail.com>
2011-12-21 11:02:24 +0200

    fetching live messages

--- mobi/js/chat.js
+++ mobi/js/chat.js
@@ -2,12 +2,7 @@ var Chat = (function() {
 
     var error_sleep_time = 500;
 
-    var Chat = function() {
-
-
-
-
-    };
+    var Chat = function() {};
     Chat.fn = Chat.prototype;
 
 
@@ -31,59 +26,28 @@ var Chat = (function() {
         })
     };
 
+    Chat.fn.live_messages = function(url) {
+        var that = this;
+        $.ajax({
+            url: url,
+            success: function(r) {
+                that.draw_messages(r)
+
+
+                window.setTimeout(function() {that.live_messages(url)}, 1000);
+            },
+
+            error: function(r) {
+                error_sleep_time *= 2;
+                console.log('Poll error, sleeping for', error_sleep_time, 'ms');
+                window.setTimeout(function() { that.live_messages(url) }, error_sleep_time);
+            }
+        });
+
+    }
+
 
 
     return Chat;
 
 })();
-
-
-var updater = {
-    errorSleepTime: 500,
-    cursor: null,
-
-    poll: function() {
-        var args = {"_xsrf": getCookie("_xsrf")};
-        if (updater.cursor) args.cursor = updater.cursor;
-        $.ajax({url: "/a/message/updates", type: "POST", dataType: "text",
-                data: $.param(args), success: updater.onSuccess,
-                error: updater.onError});
-    },
-
-    onSuccess: function(response) {
-        try {
-            updater.newMessages(eval("(" + response + ")"));
-        } catch (e) {
-            updater.onError();
-            return;
-        }
-        updater.errorSleepTime = 500;
-        window.setTimeout(updater.poll, 0);
-    },
-
-    onError: function(response) {
-        updater.errorSleepTime *= 2;
-        console.log("Poll error; sleeping for", updater.errorSleepTime, "ms");
-        window.setTimeout(updater.poll, updater.errorSleepTime);
-    },
-
-    newMessages: function(response) {
-        if (!response.messages) return;
-        updater.cursor = response.cursor;
-        var messages = response.messages;
-        updater.cursor = messages[messages.length - 1].id;
-        console.log(messages.length, "new messages, cursor:", updater.cursor);
-        for (var i = 0; i < messages.length; i++) {
-            updater.showMessage(messages[i]);
-        }
-    },
-
-    showMessage: function(message) {
-        var existing = $("#m" + message.id);
-        if (existing.length > 0) return;
-        var node = $(message.html);
-        node.hide();
-        $("#inbox").append(node);
-        node.slideDown();
-    }
-};
