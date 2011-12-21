5ec853e50f13bb6b710b1bdd76d23539f2dd5512
Simon de Haan <simon@praekeltfoundation.org>
2011-12-21 12:21:28 +0200

    vumiapp tests

--- qawa/test_vumiapp.py
+++ qawa/test_vumiapp.py
@@ -1,12 +1,42 @@
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, returnValue
 from vumi.application.tests.test_base import ApplicationTestCase
-from qawa.vumiapp import QawaApplication
+from qawa.vumiapp import QawaApplication, DEFAULT_GROUP_NAME
 
 class VumiappTestCase(ApplicationTestCase):
 
     application_class = QawaApplication
+    msisdn = '27123456789'
 
     @inlineCallbacks
-    def test_something(self):
+    def fake_incoming(self, content):
         application = yield self.get_application({})
-        print application
+        msg = self.mkmsg_in(content=content)
+        # submit a message to the application
+        self.dispatch(msg)
+        # check the application's response
+        [msg] = yield self.wait_for_dispatched_messages(1)
+        returnValue(msg)
+
+    @inlineCallbacks
+    def test_add_to_default_group(self):
+        msg = yield self.fake_incoming('+%s' % (self.msisdn,))
+        self.assertEqual(msg['content'], '+%s added to %s' %
+                            (self.msisdn, DEFAULT_GROUP_NAME))
+
+    @inlineCallbacks
+    def test_add_to_named_group(self):
+        msg = yield self.fake_incoming('#coffeelovers +%s' % (self.msisdn,))
+        self.assertEqual(msg['content'], '+%s added to %s' %
+                            (self.msisdn, 'coffeelovers'))
+
+    @inlineCallbacks
+    def test_remove_from_default_group(self):
+        msg = yield self.fake_incoming('-%s' % (self.msisdn,))
+        self.assertEqual(msg['content'], '+%s removed from %s' %
+                            (self.msisdn, DEFAULT_GROUP_NAME))
+
+    @inlineCallbacks
+    def test_remove_from_named_group(self):
+        msg = yield self.fake_incoming('#coffeelovers -%s' % (self.msisdn,))
+        self.assertEqual(msg['content'], '+%s removed from %s' %
+                            (self.msisdn, 'coffeelovers'))
--- qawa/vumiapp.py
+++ qawa/vumiapp.py
@@ -35,6 +35,7 @@ class QawaApplication(ApplicationWorker):
         except UserStore.RecordNotFound:
             user = user_store.create(msisdn, {
                 'name': name,
+                'msisdn': msisdn,
             })
 
         # Update the group details
@@ -49,9 +50,11 @@ class QawaApplication(ApplicationWorker):
             group = group_store.find(group_name)
             if group.is_member(user):
                 user.leave_group(group_name)
-                return '%s removed from %s' % (user.get('name', msisdn), group_name)
+                return '%s removed from %s' % (
+                            user.get('name') or msisdn, group_name)
             else:
-                return '%s not a member of %s' % (user.get('name', msisdn), group_name)
+                return '%s not a member of %s' % (
+                            user.get('name') or msisdn, group_name)
         except UserStore.RecordNotFound:
             log.err()
             return 'User %s not found' % (msisdn,)
